613035a0e1f5b4aae7aa6ffe55702d6638bd9300
addshore <addshorewiki@gmail.com>
2019-09-05 09:36:06 -0500

    qsgateway, test for inline URIs & 2 on a line

--- src/app/RequestParser.php
+++ src/app/RequestParser.php
@@ -19,19 +19,19 @@ class RequestParser{
      * TODO we should track which case is hit the most to decide the order they should be executed?
      */
     public static function getExternalInternalHosts( $query, $serverArray ) {
-      // TODO use strpos first to see if any prefixes actually appear?
-      $prefixSuccess = preg_match_all( '/(?:.*)PREFIX(?:.*):(?:.*)<(https?:\/\/(.+?))\/(?:entity|prop|reference|value|wiki)\/?(?:.*)>/i', $query, $prefixMatch );
-      $prefixDomains = $prefixMatch[2];
+      // TODO use strpos first to see if any uris actually appear?
+      $uriSuccess = preg_match_all( '/\<(https?:\/\/([^\>]+))\/(?:entity|prop|reference|value|wiki)(?:[^\<]+)\>/i', $query, $uriMatch );
+      $uriDomains = $uriMatch[2];
 
-      if($prefixSuccess){
+      if($uriSuccess){
 
-        // HTTP_HOST and PREFIX match
+        // HTTP_HOST and URI match
         // Try to figure out the correct taregt by looking at where the request came from
-        // and the headers used in the request..
+        // and the uris used in the request..
         if( array_key_exists( 'HTTP_HOST', $_SERVER ) && $_SERVER['HTTP_HOST'] ) {
           $requestHost = $_SERVER['HTTP_HOST'];
-          if( ( $key = array_search( $requestHost, $prefixDomains ) ) !== false ) {
-            return[ $prefixDomains[$key], self::getInternalHostFromExternal( $prefixDomains[$key] ) ];
+          if( ( $key = array_search( $requestHost, $uriDomains ) ) !== false ) {
+            return[ $uriDomains[$key], self::getInternalHostFromExternal( $uriDomains[$key] ) ];
           }
         }
 
