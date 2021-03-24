93a41d2beae612c7ead35bf4f93a4677d45d8aa4
addshore <addshore@users.noreply.github.com>
2021-03-24 17:44:45 +0000

    New tests (#10)
    * Tests for resolver.domainForResolver
    
    * Only pass 1 param to domainForResolver
    
    * Add a basic null test for resolver.defaultResolver

--- test/domainForResolver.js
+++ test/domainForResolver.js
@@ -1,11 +1,47 @@
 const assert = require('assert');
 var r = require("./../resolver.js")
 
-describe('resolver', function() {
-    it('null test', function() {
+describe('resolver.domainForResolver', function() {
+    it('nothing', function() {
         assert.strictEqual(
-            null,
+            r.domainForResolver({headers:{}}),
             null
+        );
+    });
+    it('host', function() {
+        assert.strictEqual(
+            r.domainForResolver({headers:{'host': 'somexhost'}}),
+            "somexhost"
+        );
+    });
+    it('host sub domain not removed', function() {
+        assert.strictEqual(
+            r.domainForResolver({headers:{'host': 'somename.somexhost'}}),
+            "somename.somexhost"
+        );
+    });
+    it('host www removed', function() {
+        assert.strictEqual(
+            r.domainForResolver({headers:{'host': 'www.somexhost'}}),
+            "somexhost"
+        );
+    });
+    it('x-forwarded-host', function() {
+        assert.strictEqual(
+            r.domainForResolver({headers:{'x-forwarded-host': 'somexhost'}}),
+            "somexhost"
+        );
+    });
+    it('x-forwarded-host sub domain not removed', function() {
+        assert.strictEqual(
+            r.domainForResolver({headers:{'x-forwarded-host': 'somename.somexhost'}}),
+            "somename.somexhost"
+        );
+    });
+    it('x-forwarded-host www removed', function() {
+        assert.strictEqual(
+            r.domainForResolver({headers:{'x-forwarded-host': 'www.somexhost'}}),
+            "somexhost"
         );
     });
 });
